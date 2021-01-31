bdfeed68a17571d3cea489fbe19a3f369a273652
= <dominik.kesim@gmail.com>
2021-01-31 15:57:36 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import NavBar from './components/layout/NavBar';
+
+
+class App extends React.Component {
+
+    render() {
+        return(
+            <div>
+                <NavBar />
+            </div>
+        );
+    }
+}
+
+export default App;
