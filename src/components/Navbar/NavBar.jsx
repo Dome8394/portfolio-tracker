1535e3e9cdd72293f4383cc8ac35671077e88b72
= <dominik.kesim@gmail.com>
2021-02-27 20:08:52 +0100

    Introduced and tried another color scheme

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -10,7 +10,7 @@ class NavBar extends React.Component {
     render() {
         return (
             <div>
-                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <nav className="navbar navbar-expand-lg navbar-light navbar-bg">
                     <div className="container-fluid">
                         <a className="navbar-brand">Portfolio Tracker</a>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
